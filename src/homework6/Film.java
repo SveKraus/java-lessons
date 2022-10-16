9627b67e219fb5b57d87c63ce862444f3f4adbdd
SveKraus <kraus.svetlana@gmx.de>
2022-10-16 21:14:21 +0200

    initial commit

---
+++ src/homework6/Film.java
@@ -0,0 +1,21 @@
+package homework6;
+
+import java.util.Scanner;
+
+public class Film {
+    public static void main(String[] args) {
+        boolean decision = dressCode();
+        if (decision){
+            System.out.println("Можно идти смотреть кино");
+        }
+        else
+            System.out.println("Нельзя идти смотреть кино");
+    }
+    static boolean dressCode () {
+        System.out.print("Сколько тебе лет?: ");
+        Scanner age = new Scanner(System.in);
+        int ageOfHuman = age.nextInt();
+        return ageOfHuman >= 18;
+    }
+
+}
